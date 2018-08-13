27ac84f8a5f72a685da0d10fa88e8d1d8a3161f3
Ian McInerney <I.McInerney17@imperial.ac.uk>
2018-08-13 18:34:51 +0100

    Added help listing

---
+++ matrixGeneration/condensedFormulation/Contents.m
@@ -0,0 +1,10 @@
+% Condensed Formulation
+%
+% This folder contains functions related to the creation of matrices for
+% the condensed matrices in MPC.
+%
+% Linear-Time-Invariant matrix generation functions:
+%   condensed_hessian_gen    - Generate the condensed Hessian matrix
+%   condensed_initial_gen    - Generate the initial state mapping matrix
+%   condensed_linear_gen     - Generate the condensed matrix for the linear term
+%   condensed_prediction_gen - Generate the full condensed prediction matrix
